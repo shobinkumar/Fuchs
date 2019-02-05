0e3d253af097c7116e2c5c092649717663da1bd5
jeet.htis@gmail.comgit config --global user.email jeet.htis@gmail.com <jatinder.josh@gamil.com>
2019-02-05 12:38:28 +0530

    init commit

--- app/src/main/java/com/fuchs/MainActivity.java
+++ app/src/main/java/com/fuchs/MainActivity.java
@@ -2,6 +2,8 @@ package com.fuchs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show();
     }
 }
